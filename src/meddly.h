11df8fcddc2ce61bacbbf875cbd5b774eb2adc4d
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2019-03-21 10:14:32 +0100

    downsize with possible loss

--- src/meddly.h
+++ src/meddly.h
@@ -855,7 +855,7 @@ class MEDDLY::output {
           @throws     An appropriate error
     */
     virtual void put(long x, int w=0) = 0;
-
+	
     /**
         Write an unsigned, decimal integer to the output stream.
           @param  x   Integer to write
@@ -920,7 +920,7 @@ inline  MEDDLY::output& operator<< (MEDDLY::output &s, int x)             { s.pu
 inline  MEDDLY::output& operator<< (MEDDLY::output &s, long x)            { s.put(x); return s; }
 inline  MEDDLY::output& operator<< (MEDDLY::output &s, unsigned long x)   { s.put(x); return s; }
 inline  MEDDLY::output& operator<< (MEDDLY::output &s, double x)          { s.put(x); return s; }
-inline  MEDDLY::output& operator<< (MEDDLY::output &s, size_t x)   		  { s.put(x); return s; }
+inline  MEDDLY::output& operator<< (MEDDLY::output &s, size_t x)   		  { s.put((unsigned long)x); return s; }
 
 
 // ******************************************************************
