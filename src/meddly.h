e322ec14115f7e65636a8c6ebd75811bfef24eeb
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-04-22 16:16:21 +0200

    redundant declaration
    size_t conflicts with unsigned long on some platforms

--- src/meddly.h
+++ src/meddly.h
@@ -916,7 +916,6 @@ inline  MEDDLY::output& operator<< (MEDDLY::output &s, int x)             { s.pu
 inline  MEDDLY::output& operator<< (MEDDLY::output &s, long x)            { s.put(x); return s; }
 inline  MEDDLY::output& operator<< (MEDDLY::output &s, size_t x)   { s.put(x); return s; }
 inline  MEDDLY::output& operator<< (MEDDLY::output &s, unsigned x)        { s.put((unsigned long)x); return s; }
-inline  MEDDLY::output& operator<< (MEDDLY::output &s, unsigned long x)   { s.put(x); return s; }
 inline  MEDDLY::output& operator<< (MEDDLY::output &s, double x)          { s.put(x); return s; }
 
 
